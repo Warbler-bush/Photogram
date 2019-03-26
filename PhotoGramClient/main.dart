56b4aff8555b76b54394a1d6885c9ce7b190da6a
lucaerba <43878427+lucaerba@users.noreply.github.com>
2019-03-26 14:59:12 +0100

    Add files via upload

--- PhotoGramClient/main.dart
+++ PhotoGramClient/main.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'dart:io';
-import 'package:english_words/english_words.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 void main() => runApp(TabBarDemo());
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+  final _suggestions = {"ciao": "cacca"};
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-          // restituisce il numero di parole
-          final index = i ~/ 2; /*3*/
-          // controlla se il numero di parole visualizzabili
-          // sono maggiori di quelle visualizzate
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
+      return ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: /*1*/ (context, i) {
+              if (i.isOdd) return Divider(); /*2*/
+              // restituisce il numero di parole
+              final index = i ~/ 2; /*3*/
+              // controlla se il numero di parole visualizzabili
+              // sono maggiori di quelle visualizzate
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(_suggestions); /*4*/
+              }
+              return _buildRow();
+          });
   }
 
-  Widget _buildRow(WordPair pair) {
+  Widget _buildRow() {
+    String pair = "ciao";
     return ListTile(
       title: Text(
-        pair.asPascalCase,
+        pair,
         style: _biggerFont,
       ),
     );
@@ -55,10 +56,9 @@ class RandomWords extends StatefulWidget {
 
 class TabBarDemo extends StatelessWidget {
   void send(){
-    String txt = ScrollableArea.mycontroller.text;
-    //TODO: fare formattazione json
+    Message m = new Message(ScrollableArea.mycontroller.text, "it", "g");
     Client c = new Client();
-    c.connetti("10.0.2.2", 1235, txt); //10.0.2.2 local ip of machine
+    c.connetti("185.51.138.58", 1235, m.toJson()); //10.0.2.2 local ip of machine
   }
 
   @override
@@ -83,7 +83,7 @@ class TabBarDemo extends StatelessWidget {
               children: [
                 // aggiungi le interfaccie associate qua sotto, corrispondo alle tab
                 ScrollableArea(),
-                Icon(Icons.directions_bike),
+                Photo(),
                 RandomWords(),
               ],
             ),
@@ -108,9 +108,9 @@ class ScrollableArea extends StatelessWidget {  //classe che permetettte di crea
   @override
   Widget build(BuildContext context) {
     return new SizedBox.expand(
-          child: new Container(
-            //height: MediaQuery.of(context).size.height/2,
-            padding: const EdgeInsets.all(8.0),
+        child: new Container(
+          //height: MediaQuery.of(context).size.height/2,
+          padding: const EdgeInsets.all(8.0),
 
           //padding: EdgeInsets.all(8.0),
           child: TextField(
@@ -127,8 +127,52 @@ class ScrollableArea extends StatelessWidget {  //classe che permetettte di crea
                 hintStyle: new TextStyle(color: Colors.grey[800]),
                 hintText: "Inserisci un testo da analizzare",
                 fillColor: Colors.white70),
+
           ),
-      )
+        )
+    );
+
+  }
+
+}
+
+class Photo extends StatelessWidget {  //classe che permetettte di creare widget personalizzzato TODO:guardare utilizzo di StateFullWidget
+  static TextEditingController mycontroller = new TextEditingController();
+
+  Photo({Key key,});
+  File immagine;
+  
+  Future pickImage() async {
+    var tempStorage = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    immagine = tempStorage;
+
+    FirebaseVisionImage image = FirebaseVisionImage.fromFile(immagine);
+    TextRecognizer recognizer = FirebaseVision.instance.textRecognizer();
+    VisionText readText = await recognizer.processImage(image);
+    var testo = "";
+
+    for(TextBlock block in readText.blocks)
+      for(TextLine line in block.lines)
+        for(TextElement element in line.elements)
+          testo += element.text;
+
+    ScrollableArea.mycontroller.text = readText.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizedBox.expand(
+        child: new Container(
+          //height: MediaQuery.of(context).size.height/2,
+          padding: const EdgeInsets.all(8.0),
+
+          //padding: EdgeInsets.all(8.0),
+          child: RaisedButton(
+            child: Text("Prendi testo dall'immagine"),
+            onPressed: pickImage,
+          ),
+        )
     );
 
   }
@@ -156,7 +200,7 @@ class Client{
 
       //invio
       print("ok");
-      socket.write(this.text); //'{"lang":"it","type":"g","text":"A Wang piacciono i cani"}'
+      socket.write(text); //'{"lang":"it","type":"g","text":"A Wang piacciono i cani"}'
 
       //ricevo
       socket.listen((data){
@@ -171,8 +215,25 @@ class Client{
   }
 }
 
+class Message {
+  final String text;
+  final String lang;
+  final String type;
 
+  Message(this.text, this.lang, this.type);
 
+  Message.fromJson(Map<String, dynamic> json)
+      : text = json['text'],
+        lang = json['lang'],
+        type = json['type'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        'text': text,
+        'lang': lang,
+        'type': type,
+      };
+}
 
 
 
